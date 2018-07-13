476604fbc2e79f2c50cee095f7d1bf7d57d84865
Jonghyun Lee <99th_day@naver.com>
2018-07-13 17:58:08 +0900

    장고 프로젝트 튜토리얼 마무리
    장고 프로젝트 튜토리얼 마무리

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     re_path(r'^$', views.post_list, name='post_list'),
+    re_path(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    re_path(r'^post/new/$', views.post_new, name='post_new'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 
 # Create your views here.
@@ -11,4 +12,31 @@ def post_list(request):
 
     return render(request, 'blog/post_list.html', {
         'post_list' : qs,
+    })
+
+def post_detail(request, pk):
+    # pk = "100"
+    post = get_object_or_404(Post, pk=pk)
+    # try:
+    #    post = Post.objects.get(pk=pk)
+    # except Post.DoesNotExist:
+    #   raise Http404   # Page Not Found : from django.http import Http404
+    return render(request, 'blog/post_detail.html', {
+        'post' : post,
+    })
+
+def post_new(request):
+    # request.POST, request.FILES
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {
+        'form' : form,
     })
