753fd9238e6b101d8826536404aad8b2a12d5ca4
Jonghyun Lee <99th_day@naver.com>
2018-07-14 20:47:54 +0900

    튜토리얼 코드 및 버그 수정
    튜토리얼 코드 수정

--- blog/views.py
+++ blog/views.py
@@ -1,62 +1,45 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import get_object_or_404, redirect, render
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 
 
-# Create your views here.
 def post_list(request):
-    qs = Post.objects.all()
-    qs = qs.filter(published_date__lte=timezone.now())
-    qs = qs.order_by('published_date')
-
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {
-        'post_list' : qs,
+        'posts': posts,
     })
+
 
 def post_detail(request, pk):
-    # pk = "100"
     post = get_object_or_404(Post, pk=pk)
-    # try:
-    #    post = Post.objects.get(pk=pk)
-    # except Post.DoesNotExist:
-    #   raise Http404   # Page Not Found : from django.http import Http404
-    return render(request, 'blog/post_detail.html', {
-        'post' : post,
-    })
+    return render(request, 'blog/post_detail.html', {'post': post})
+
 
 def post_new(request):
-    # request.POST, request.FILES
-
-    if request.method == 'POST':
-        form = PostForm(request.POST, request.FILES)
+    if request.method == "POST":
+        form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
-            return redirect('post_detail', post.pk)
+            return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
-    return render(request, 'blog/post_edit.html', {
-        'form' : form,
-    })
+    return render(request, 'blog/post_edit.html', {'form': form})
 
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
-
-    if request.method == 'POST':
-        form = PostForm(request.POST, request.FILES, instance=post)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
-            return redirect('post_detail', post.pk)
+            return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
-
-    return render(request, 'blog/post_edit.html', {
-        'form' : form,
-    })
+    return render(request, 'blog/post_edit.html', {'form': form})
