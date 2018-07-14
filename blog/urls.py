9b1ef39ecdb98de236a3377c91f14e241565dcb8
Jonghyun Lee <99th_day@naver.com>
2018-07-14 12:46:37 +0900

    장고걸스 기본 튜토리얼 끝
    장고걸스 기본 튜토리얼 끝

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     re_path(r'^$', views.post_list, name='post_list'),
     re_path(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     re_path(r'^post/new/$', views.post_new, name='post_new'),
+    re_path(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
@@ -35,8 +35,28 @@ def post_new(request):
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
+            return redirect('post_detail', post.pk)
     else:
         form = PostForm()
+    return render(request, 'blog/post_edit.html', {
+        'form' : form,
+    })
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', post.pk)
+    else:
+        form = PostForm(instance=post)
+
     return render(request, 'blog/post_edit.html', {
         'form' : form,
     })
