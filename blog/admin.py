864f480a4f15aab23c43af378ffbdd688cae8d0d
Jonghyun Lee <99th_day@naver.com>
2018-07-20 10:07:50 +0900

    commit
    commit

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Post, Comment
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'created_date', 'published_date']
+
+#admin.site.register(Post, PostAdmin) # 같은 표현
+
+
 admin.site.register(Comment)
--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,25 @@
+import re
+from django.forms import ValidationError
 from django.db import models
 from django.utils import timezone
 
 
+def lnglat_validator(value):
+    if not re.match(r'^([+-]?\d+\.?\d*),([+-]?\d+\.?\d*)$', value):
+        raise ValidationError('Invalid LngLat Type')
+
+
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
+    title = models.CharField(max_length=100, verbose_name='제목',
+        help_text='포스팅 제목을 입력해주세요. 최대 100자 내외')
+    text = models.TextField(verbose_name='내용')
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    tags = models.CharField(max_length=100, blank=True)
+    lnglat = models.CharField(max_length=50,
+        validators=[lnglat_validator],
+        blank=True, help_text='경도/위도 포맷으로 입력')
 
     def publish(self):
         self.published_date = timezone.now()
