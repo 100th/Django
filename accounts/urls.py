f6ac0fe33badd3494d0492796b5fb769a7e21146
Jonghyun Lee <99th_day@naver.com>
2018-07-31 15:22:43 +0900

    회원가입 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, re_path
 from django.contrib.auth.views import login, logout
-from .views import profile
+from .views import profile, signup
 
 urlpatterns = [
-    re_path(r'^login/$', login, name='login', kwargs={ 'template_name' : 'accounts/login.html', }),
+    re_path(r'^signup/$', signup, name='signup'),
     re_path(r'^profile/$', profile, name='profile'),
+    re_path(r'^login/$', login, name='login', kwargs={ 'template_name' : 'accounts/login.html', }),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 
 @login_required
@@ -7,4 +9,17 @@ def profile(request):
     # order_list = Order.objects.filter(user=request.user) 위랑 같음
     return render(request, 'accounts/profile.html', {
         'order_list' : order_list,
+    })
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(settings.LOGIN_URL) # 회원가입에 성공하면, LOGIN 페이지로 이동
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup_form.html', { # 템플릿은 일반적인 form template
+        'form': form,
     })
