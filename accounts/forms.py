8c08d669858cdefd6911ad98418009e7421d601d
Jonghyun Lee <99th_day@naver.com>
2018-08-12 17:33:19 +0900

    회원가입 시 중복 이메일 사용 금지 구현

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from .models import Profile
 
@@ -6,8 +7,18 @@ class SignupForm(UserCreationForm):
     phone_number = forms.CharField()
     address = forms.CharField()
 
+
     class Meta(UserCreationForm.Meta):
         fields = UserCreationForm.Meta.fields + ('email',)
+
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email', '')
+        if email:
+            if get_user_model().objects.filter(email=email).exists():
+                raise forms.ValidationError('duplicated email')
+        return email
+
 
     def save(self):
         user = super().save()
@@ -20,6 +31,7 @@ class SignupForm(UserCreationForm):
 
 class LoginForm(AuthenticationForm):
     answer = forms.IntegerField(label='3+3=?')
+
 
     def clean_answer(self):
         if self.cleaned_data.get('answer', None) != 6:
